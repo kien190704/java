970f744d085f1f7fe02160a24ac4b131596d0d64
kien190704 <127111199+kien190704@users.noreply.github.com>
2023-12-13 20:36:27 +0700

    Add files via upload

---
+++ HelloWorldMultilingual.java
@@ -0,0 +1,8 @@
+public class HelloWorldMultilingual {
+    public static void main(String[] args) {
+       System.out.println("Hello world"); // english
+       System.out.println("Bonjour le monde"); // french
+       System.out.println("xin chào thế giới"); // tiếng việt
+       System.out.println("Buenos días, mundo"); // spanish
+     }
+}
---
+++ TenHelloWorld.java
@@ -0,0 +1,10 @@
+public class TenHelloWorld {
+   public static void main(String[] args) {
+      for(int i =0; i<=10;i++){
+         System.out.println("Hello, World");
+      }
+   }
+}
+// vao folder mo comand
+// javac ten fiel roi tab duoi java
+// java ten file duoi java
